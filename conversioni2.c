4818060f2f9fc6e32e1ebea67a9d8fc9768cf941
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-05-25 11:28:41 +0200

    studio delle funzioni

--- conversioni2.c
+++ conversioni2.c
@@ -2,99 +2,93 @@
 
 #include <stdio.h>
 void conversione(int n, int base);
+void stampabit(int bit);
 
 
 int main()
 {
-    int n, base;
-    printf("inserisci un valore compreso tra 0 e 255");
+    int n, scelta, base;
+    printf("inserisci un valore compreso tra 0 e 255\n");
 
     do{
         scanf("%d", &n);
     }while (n <=  0 || n > 255);
 
-    printf("scegli il tipo di conversione che vuoi eseguire\n");
-    printf("scegli la base");
-    scanf("%d", &base);
-    conversione(n, base);
+    printf("scegli il tipo di conversione che vuoi eseguire \n");
+    printf("1 binario : 2 ottale : 3 esadecimale");
+    scanf("%d", &scelta);
+    switch(scelta)
+    {
+        case 1:
+            conversione(n, 2);
+        break;
+        case 2:
+            conversione(n, 8);
+        break;
+        case 3:
+            conversione(n, 16);
+        break;
+        default:
+            printf("sei ritardato");
+        break;
+    }
 
     return 0;
 }
 
-
-
 void conversione(int n, int base)
 {
-    int peso, bit, i;
+    int peso, bit, numerobit, i;
     switch(base)
     {
         case 2:
             peso = 128;
- 
-            for(i = 0; i < 8; i++)
-            {
-               bit = n / peso;
-               printf("%d", bit);
-               n -= bit * peso;
-               peso /= 2;
-            }
-
+            numerobit = 8;
         break;
         case 8:
-           peso = 64;
-
-           for(i = 0; i < 3; i++)
-           {
-              bit = n / peso;
-              printf("%d", bit);
-              n -= bit * peso;
-              peso /= 8; 
-           }
-
+            peso = 64;
+            numerobit = 3;
         break;
-
         case 16:
-            
-           peso = 16;
-           for(i = 0; i < 2; i++)
-           {
-              bit = n / peso;
-        
-                 switch(bit)
-                 {
-                    case 10:
-                    putchar('A');
-                    break;
-                    case 11:
-                    putchar('B');
-                    break;
-                    case 12:
-                    putchar('C');
-                    break;
-                    case 13:
-                    putchar('D');
-                    break;
-                    case 14:
-                    putchar('E');
-                    break;
-                    case 15:
-                    putchar('F');
-                    break;
-                    default:
-                    printf("%d", bit);
-                    break;
-                 }
-    
-                 n -= bit * peso;
-                 peso /= 16;
-           }
-            
-        break;
-        default:
-            ("sei ritardato");
+            peso = 16;
+            numerobit = 2;
         break;
     }
-
+            
+    for(i = 0; i < numerobit; i++)
+    {
+        bit = n / peso;
+        n -= bit * peso;
+        peso /= base; 
+        stampabit(bit);
+    }
 }
 
 
+void stampabit(int bit)
+{
+        switch(bit)
+        {
+            case 10:
+                putchar('A');
+            break;
+            case 11:
+                putchar('B');
+            break;
+            case 12:
+                putchar('C');
+            break;
+            case 13:
+                putchar('D');
+            break;
+            case 14:
+                putchar('E');
+            break;
+            case 15:
+                putchar('F');
+            break;
+            default:
+                printf("%d", bit);
+            break;
+        }
+}
