ab3e62e8836b14ffd9ee8e4c791b1d2ac9f90f07
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-10-28 19:20:15 +0100

    correzione

--- dado/main.c
+++ dado/main.c
@@ -5,31 +5,17 @@ void stampa(int occorrenze, int faccia);
 
 int main()
 {
-    int i = 0, risultato_lancio, facce[] = {0, 0, 0, 0, 0, 0};
+    int i = 0, risultato_lancio, facce[] = {0, 0, 0, 0, 0, 0, 0};
 
     lancio_init();
 
     for(i = 0; i < 100; i++)
     {
         risultato_lancio = lancio_dado();
-        switch(risultato_lancio)
-        {
-            case 1:    facce[0]++;
-            break;
-            case 2:    facce[1]++;
-            break;
-            case 3:    facce[2]++;
-            break;
-            case 4:    facce[3]++;
-            break;
-            case 5:    facce[4]++;
-            break;
-            case 6:    facce[5]++;
-            break;
-        }
+        facce[risultato_lancio]++;
     }
 
-        for(i = 0; i < 6; i++)
+        for(i = 1; i < 7; i++)
         {
             stampa(facce[i], i);
         }
@@ -41,7 +27,7 @@ int main()
 void stampa(int occorrenze, int faccia)
 {
     int i;
-    printf("%d #%d", faccia + 1, occorrenze);
+    printf("%d #%d", faccia, occorrenze);
     for(i = 0; i < occorrenze; i++)    printf("*");
     puts("\n");
 }
